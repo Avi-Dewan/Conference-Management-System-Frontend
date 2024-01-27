836b295905008aea21aea4c3b062bc6f1df235b8
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-01-27 23:14:52 +0600

    user login added ( need CSS)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,184 @@
   import Navbar from "/src/components/navbar.svelte";
   import { goto } from "$app/navigation";
 
-  function handleSubmit() {
-    let user_id = 1;
-    goto(`/${user_id}/home`);
+  let email, password, user_id;
+
+  async function handleLogIn() {
+    const response = await fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"email": email, "password": password}),
+    });
+
+    let data = await response.json();
+    user_id = data.user_id;
+    
+    if(user_id) {
+      goto(`/${user_id}/home`);
+    } else {
+      alert("Wrong password")
+    }
+
   }
 </script>
 
 <main>
-  <button on:click={handleSubmit}></button>
+
+  <div class="form">
+    <div class="form-control">
+      <label for="email">Email:</label>
+      <input
+        type="text"
+        id="email"
+        bind:value={email}
+      />
+    </div>
+
+    <div class="form-control">
+      <label for="pasword">Password:</label>
+
+      <input
+        type="password"
+        id="password"
+        bind:value={password}
+      />
+    </div>
+
+  </div>
+
+  <div class="form-control" style="display: block;">
+    <button on:click={handleLogIn}>Log in</button>
+  </div>
 </main>
 
+
+
 <style>
+  .two-column {
+    column-count: 2; /* Split into two columns */
+    column-gap: 30%; /* Adjust gap between columns */
+  }
+  .two-column button {
+    margin-left: -10%;
+  }
+  .two-column div {
+    break-inside: avoid; /* Prevent breaking elements between columns */
+  }
+  input {
+    height: 50px;
+    width: 500px;
+  }
   main {
     max-width: 90%;
     margin: 2% auto;
   }
 
   h1 {
+  }
+
+  .form {
+    width: 900px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    background-color: #f9f9f9;
+    float: left;
+  }
+
+  .form-control {
+    margin-bottom: 20px;
+    min-height: 20px;
+    min-width: 50px;
+    clear: both;
+  }
+
+  .form-control label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+
+  .form-control input,
+  .form-control select {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  .form-control button {
+    margin-top: 5%;
+    padding: 8px 20px;
+    border: none;
+    border-radius: 4px;
+    background-color: #007bff;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  .form-control button:hover {
+    background-color: #0056b3;
+  }
+
+  .form-control-submission {
+    margin-bottom: 20px;
+  }
+
+  .form-control-submission label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+
+  .form-control-submission input,
+  .form-control-submission select {
+    width: 95%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+  .column {
+    float: left;
+    width: 50%;
+  }
+  .column-field {
+    align-items: flex-start;
+    float: left;
+    width: 50%;
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+
+    border-radius: 8px;
+    background-color: #f9f9f9;
+    display: flex;
+    flex-direction: column;
+  }
+  .column button {
+    margin-left: 18%;
+    margin-top: 1%;
+    background-color: green;
+    height: 40px;
+  }
+
+  /* Clear floats after the columns */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  .redFont {
+    color: red;
+  }
+  .desc input {
+    height: 150px;
   }
 </style>
