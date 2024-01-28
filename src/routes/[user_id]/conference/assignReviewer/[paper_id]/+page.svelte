c918e89aa8541a348fd5c8e74e284f4bf595cebd
rakibkibria <1905098@ugrad.cse.buet.ac.bd>
2024-01-28 22:56:04 +0600

    final

--- src/routes/[user_id]/conference/assignReviewer/[paper_id]/+page.svelte
+++ src/routes/[user_id]/conference/assignReviewer/[paper_id]/+page.svelte
@@ -7,15 +7,17 @@
   const user_id = $page.params.user_id;
   const paper_id = $page.params.paper_id;
 
-  console.log(user_id);
-
   let paper_details_url = `http://localhost:3000/paper/${paper_id}`;
   let paper_author_url = `http://localhost:3000/paper/${paper_id}/author`;
   let reviewer_auto_url = `http://localhost:3000/assign/auto/${paper_id}`;
+
   let request_reviwer_url = `http://localhost:3000/assign/request`;
   let request_delete_url = `http://localhost:3000/assign/request_delete`;
+  let already_sent_request_url = `http://localhost:3000/assign/sent_request/${paper_id}`;
 
   let reviewer_manual_url = `http://localhost:3000/assign/manual/${paper_id}`;
+
+  let already_assigned_reviewer_url = `http://localhost:3000/reviewer/assigned/${paper_id}`;
 
   let paper_details = null;
   let paper_author_details = null;
@@ -27,8 +29,15 @@
   let showAuto = false;
   let authors = [];
   let suggestedReviewer = null;
+  let suggestedManualReviewer = null;
   let data = [];
   let filteredSuggestedReviewer = [];
+  let filteredManualReviewer = [];
+
+  let alreadyRequestedReviewer = null;
+  let alreadyAssignedReviewer = null;
+
+  let showManual = false;
   //paper details
   onMount(async () => {
     try {
@@ -40,7 +49,6 @@
       paper_details = await response.json();
 
       paper_details = paper_details[0];
-      console.log(paper_details);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -65,6 +73,39 @@
   //kader kader request kora hoise
   onMount(async () => {
     try {
+      const response = await fetch(already_sent_request_url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      alreadyRequestedReviewer = await response.json();
+
+      console.log(alreadyRequestedReviewer);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
+
+  //kader kader assign kora hoise
+  onMount(async () => {
+    try {
+      const response = await fetch(already_assigned_reviewer_url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      alreadyAssignedReviewer = await response.json();
+
+      console.log(alreadyAssignedReviewer);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
+
+  //kara kara suggested
+
+  onMount(async () => {
+    try {
       const response = await fetch(reviewer_auto_url);
       if (!response.ok) {
         throw new Error("Failed to fetch data");
@@ -72,38 +113,23 @@
 
       suggestedReviewer = await response.json();
       filteredSuggestedReviewer = suggestedReviewer;
+
+      console.log(suggestedReviewer);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   });
 
-  //   //kader kader assign kora hoise
-  //   onMount(async () => {
-  //     try {
-  //       const response = await fetch(url);
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch data");
-  //       }
-
-  //       data = await response.json();
-
-  //       console.log(data);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   });
-
-  //kara kara suggested
-
+  //manually kara kara
   onMount(async () => {
     try {
-      const response = await fetch(paper_author_url);
+      const response = await fetch(reviewer_manual_url);
       if (!response.ok) {
         throw new Error("Failed to fetch data");
       }
 
-      paper_author_details = await response.json();
-      paper_author_details = paper_author_details;
+      suggestedManualReviewer = await response.json();
+      filteredManualReviewer = suggestedManualReviewer;
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -137,43 +163,105 @@
   function suggestionSelect() {
     if (filteredSuggestedReviewer != null) {
       showAuto = !showAuto;
+      showManual = false;
+    }
+  }
+
+  function manualsuggestionSelect() {
+    if (filteredManualReviewer != null) {
+      showManual = !showManual;
+      showAuto = false;
     }
   }
 </script>
 
 <main>
-  {#if paper_details != null && paper_author_details != null && suggestedReviewer != null}
+  {#if paper_details != null && paper_author_details != null && suggestedReviewer != null && alreadyRequestedReviewer != null && suggestedManualReviewer != null && alreadyAssignedReviewer != null}
     <NavbarChair />
     <div>
       <h1>Title: {paper_details.paper_title}</h1>
       <h3>Authors: {paper_author_details}</h3>
       <h3>Related Field: {paper_details.related_fields}</h3>
     </div>
+    <div>
+      <h1 style="color:red">Already Requested Reviewer</h1>
+      <div>
+        <div class="two-column">
+          {#each alreadyRequestedReviewer as item, index (item.user_id)}
+            <div>
+              <h3>Name: {item.full_name}</h3>
+              <h4>Expertise: {item.expertise}</h4>
+              <h4>Affliation: {item.current_institution}</h4>
 
-    {#if showAuto == true}
-      <div class="two-column">
-        {#each filteredSuggestedReviewer as item, index (item.user_id)}
-          <div class="two-column">
-            <div>
-              <label style="width: 1px;"><h4>{item.full_name}</h4></label>
-            </div>
-            <div>
               <button
                 on:click={() => removeRequest(paper_id, item.user_id)}
-                style="background-color:red;"
+                style="background-color:red;margin-left:0%"
               >
-                Request</button
+                Delete Request</button
               >
             </div>
+          {/each}
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <h1 style="color: green;">Already Assigned Reviewer</h1>
+
+      <div class="two-column" style="margin-left: 0px;">
+        {#each alreadyAssignedReviewer as item, index (item.user_id)}
+          <div>
+            <h3>Name: {item.full_name}</h3>
+            <h4>Expertise: {item.expertise}</h4>
+            <h4>Affliation: {item.current_institution}</h4>
           </div>
         {/each}
       </div>
-    {/if}
+    </div>
+    <div style="margin-top: 10%;">
+      <h2>Reviewer Suggestion:</h2>
+      <div class="form-control">
+        {#if showAuto == true}
+          <div class="scrollable-window">
+            <input
+              type="text"
+              placeholder="Filter by name"
+              bind:value={filterText}
+              style="max-width: 80%;margin-left:5%"
+              on:input={() => {
+                filteredSuggestedReviewer = suggestedReviewer.filter(
+                  (item) =>
+                    item.full_name
+                      .toLowerCase()
+                      .toLowerCase()
+                      .indexOf(filterText.toLowerCase()) !== -1
+                );
+
+                if (filterText == "") {
+                  filteredSuggestedReviewer = suggestedReviewer;
+                }
+              }}
+            />
+            {#each filteredSuggestedReviewer as item (item.user_id)}
+              <div>
+                <div class="card">
+                  <h3>Name: {item.full_name}</h3>
+                  <h4>Affliation: {item.current_institution}</h4>
+                  <h4>Expertise: {item.expertise}</h4>
+                  <button on:click={requestReviewer(paper_id, item.user_id)}
+                    >Request</button
+                  >
+                </div>
+              </div>
+            {/each}
+          </div>
+        {/if}
+        <button on:click={suggestionSelect}>View Suggestion</button>
+      </div>
+    </div>
 
     <div class="form-control">
-      <h2>Reviewer Suggestion:</h2>
-
-      {#if showAuto == true}
+      {#if showManual == true}
         <div class="scrollable-window">
           <input
             type="text"
@@ -181,7 +269,7 @@
             bind:value={filterText}
             style="max-width: 80%;margin-left:5%"
             on:input={() => {
-              filteredSuggestedReviewer = suggestedReviewer.filter(
+              filteredManualReviewer = suggestedManualReviewer.filter(
                 (item) =>
                   item.full_name
                     .toLowerCase()
@@ -190,11 +278,11 @@
               );
 
               if (filterText == "") {
-                filteredSuggestedReviewer = suggestedReviewer;
+                filteredManualReviewer = suggestedManualReviewer;
               }
             }}
           />
-          {#each filteredSuggestedReviewer as item (item.user_id)}
+          {#each filteredManualReviewer as item (item.user_id)}
             <div>
               <div class="card">
                 <h3>Name: {item.full_name}</h3>
@@ -208,7 +296,7 @@
           {/each}
         </div>
       {/if}
-      <button on:click={suggestionSelect}>View Suggestion</button>
+      <button on:click={manualsuggestionSelect}>Assign Manually</button>
     </div>
   {/if}
 </main>
@@ -263,8 +351,30 @@
     font-size: 16px;
   }
 
+  button {
+    margin-top: 0%;
+    padding: 8px 20px;
+    border: none;
+    border-radius: 4px;
+    background-color: #007bff;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
   .form-control button {
     margin-top: 5%;
+    padding: 8px 20px;
+    border: none;
+    border-radius: 4px;
+    background-color: #007bff;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  button {
+    margin-top: 0%;
     padding: 8px 20px;
     border: none;
     border-radius: 4px;
@@ -344,5 +454,16 @@
     overflow-y: auto;
     border: 1px solid #ccc;
     padding: 10px;
+  }
+  .two-column {
+    margin-top: -1%;
+    column-count: 2; /* Split into two columns */
+    column-gap: 30%; /* Adjust gap between columns */
+  }
+  .two-column button {
+    margin-left: -10%;
+  }
+  .two-column div {
+    break-inside: avoid; /* Prevent breaking elements between columns */
   }
 </style>
