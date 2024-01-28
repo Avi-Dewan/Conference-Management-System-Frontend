7c00fd452302d5a6739c4d049aa8a40ca426b2a9
rakibkibria <1905098@ugrad.cse.buet.ac.bd>
2024-01-28 23:43:05 +0600

    User request added

--- src/components/navbar_user.svelte
+++ src/components/navbar_user.svelte
@@ -1,103 +1,102 @@
 <script>
-    import { page } from "$app/stores";
-  
-    let user_id;
-  
-    $: user_id = $page.params.user_id;
-  </script>
-  
-  <header>
-    <div class="container">
-      <h1 class="logo"></h1>
-  
-      <nav>
-        <ul>
-          <li><a href="/{user_id}/home">Home</a></li>
-          <li>
-            <a href="/{user_id}/conference/conference_list/all">Conferences</a>
-          </li>
-          <li><a href="#">My Request</a></li>
-          <li><a href="/{user_id}/review">My Review</a></li>
-          <li><a href="#">My Profile</a></li>
-          <li><a href="/">Log out</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-  
-  <style>
-    @import url("https://fonts.googleapis.com/css?family=Work+Sans:400,600");
-    body {
-      margin: 0;
-      background: #e4ebe1;
-      font-family: "Work Sans", sans-serif;
-      font-weight: 800;
-    }
-  
-    .container {
-      width: 90%;
-      margin: 0 auto;
-    }
-  
-    header {
-      background: #e4ebe1;
-    }
-    header::after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-  
-    .logo {
-      float: left;
-      padding: 10px 0;
-    }
-  
-    nav {
-      justify-content: space-between;
-    }
-  
-    nav ul {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-    }
-  
-    nav li {
-      display: inline-block;
-      margin-left: 2%;
-      margin-right: 7%;
-      padding-top: 2%;
-  
-      position: relative;
-    }
-  
-    nav a {
-      color: #444;
-      text-decoration: none;
-      text-transform: uppercase;
-      font-size: 14px;
-    }
-  
-    nav a:hover {
-      color: #000;
-    }
-  
-    nav a::before {
-      content: "";
-      display: block;
-      height: 5px;
-      background-color: #444;
-  
-      position: absolute;
-      top: 0;
-      width: 0%;
-  
-      transition: all ease-in-out 250ms;
-    }
-  
-    nav a:hover::before {
-      width: 100%;
-    }
-  </style>
-  
+  import { page } from "$app/stores";
+
+  let user_id;
+
+  user_id = $page.params.user_id;
+</script>
+
+<header>
+  <div class="container">
+    <h1 class="logo"></h1>
+
+    <nav>
+      <ul>
+        <li><a href="/{user_id}/home">Home</a></li>
+        <li>
+          <a href="/{user_id}/conference/conference_list/all">Conferences</a>
+        </li>
+        <li><a href="/{user_id}/Request">My Request</a></li>
+        <li><a href="/{user_id}/review">My Review</a></li>
+        <li><a href="#">My Profile</a></li>
+        <li><a href="/">Log out</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Work+Sans:400,600");
+  body {
+    margin: 0;
+    background: #e4ebe1;
+    font-family: "Work Sans", sans-serif;
+    font-weight: 800;
+  }
+
+  .container {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  header {
+    background: #e4ebe1;
+  }
+  header::after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  .logo {
+    float: left;
+    padding: 10px 0;
+  }
+
+  nav {
+    justify-content: space-between;
+  }
+
+  nav ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  nav li {
+    display: inline-block;
+    margin-left: 2%;
+    margin-right: 7%;
+    padding-top: 2%;
+
+    position: relative;
+  }
+
+  nav a {
+    color: #444;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 14px;
+  }
+
+  nav a:hover {
+    color: #000;
+  }
+
+  nav a::before {
+    content: "";
+    display: block;
+    height: 5px;
+    background-color: #444;
+
+    position: absolute;
+    top: 0;
+    width: 0%;
+
+    transition: all ease-in-out 250ms;
+  }
+
+  nav a:hover::before {
+    width: 100%;
+  }
+</style>
--- src/routes/[user_id]/Request/+page.svelte
+++ src/routes/[user_id]/Request/+page.svelte
@@ -1,127 +1,123 @@
 <script>
-    import { page } from "$app/stores";
+  import { page } from "$app/stores";
 
-    import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
-    import { goto } from "$app/navigation";
-    
-  
-    let user_id, user , data;
-  
-    user_id = $page.params.user_id;
-  
-    onMount(async () => {
-      try {
-        let url = `http://localhost:3000/request/${user_id}`;
-        
-        const response = await fetch(url);
-  
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-  
-        
-        data = await response.json();
+  import { goto } from "$app/navigation";
 
-        console.log(data);
-  
-        
-  
-      } catch (error) {
-        console.error("Error fetching data:", error);
+  import NavbarUser from "/src/components/navbar_user.svelte";
+
+  let user_id, user, data;
+
+  user_id = $page.params.user_id;
+
+  onMount(async () => {
+    try {
+      let url = `http://localhost:3000/request/${user_id}`;
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
       }
-    });
 
-    async function handleReject(paper_id) {
-      const response = await fetch("http://localhost:3000/request/delete_request", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user_id: user_id, paper_id: paper_id }),
-    });
-    const thisPage = window.location.pathname;
+      data = await response.json();
 
-    goto(`/${user_id}/home`).then(()=> goto(thisPage));
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  });
 
-
-    async function handleAccept(paper_id) {
-      let response = await fetch("http://localhost:3000/request/delete_request", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user_id: user_id, paper_id: paper_id }),
-    });
-
-    response = await fetch("http://localhost:3000/reviewer/accept" , {
+  async function handleReject(paper_id) {
+    const response = await fetch(
+      "http://localhost:3000/request/delete_request",
+      {
         method: "POST",
         headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user_id: user_id, paper_id: paper_id }),
+      }
+    );
+    const thisPage = window.location.pathname;
+
+    goto(`/${user_id}/home`).then(() => goto(thisPage));
+  }
+
+  async function handleAccept(paper_id) {
+    let response = await fetch("http://localhost:3000/request/delete_request", {
+      method: "POST",
+      headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ user_id: user_id, paper_id: paper_id }),
     });
 
+    response = await fetch("http://localhost:3000/reviewer/accept", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user_id: user_id, paper_id: paper_id }),
+    });
 
     const thisPage = window.location.pathname;
 
-    goto(`/${user_id}/home`).then(()=> goto(thisPage));
-    }
+    goto(`/${user_id}/home`).then(() => goto(thisPage));
+  }
+</script>
 
+{#if data != null}
+  <main>
+    <NavbarUser />
+    <h1>Paper For Review</h1>
 
-  </script>
-  
-  {#if data != null}
-    <main>
-      
+    <!-- <button on:click={handleClick}>Go to Another Page</button> -->
 
-  
-      <h1>Paper For Review</h1>
-
-
-
-      <!-- <button on:click={handleClick}>Go to Another Page</button> -->
-
-
-      <div class="cards" >
-        {#each data as item}
-
-        <div class = "border_style">
+    <div class="cards">
+      {#each data as item}
+        <div class="border_style">
           <h2>{item.paper_title}</h2>
           <!-- <h3>Pdf Link: {item.pdf_link}</h3> -->
-          <a href="{item.pdf_link}"
-            >View Paper</a
-          >
+          <a href={item.pdf_link}>View Paper</a>
           <h3>Related Fields: {item.related_fields}</h3>
           <h3>Abstract: {item.abstract}</h3>
-          <a href="/{user_id}/conference/conference_list/all/{item.conference_id}"
+          <a
+            href="/{user_id}/conference/conference_list/all/{item.conference_id}"
             >View Conference</a
           >
 
-          <div class="two-column" style="display: block;margin-top:2%" button-container >
-            <button on:click={handleReject(item.paper_id)} style="background-color:red;">Reject</button>
-            <button on:click={handleAccept(item.paper_id)} style="background-color:green;">Accept</button>
+          <div
+            class="two-column"
+            style="display: block;margin-top:2%"
+            button-container
+          >
+            <button
+              on:click={handleReject(item.paper_id)}
+              style="background-color:red;">Reject</button
+            >
+            <button
+              on:click={handleAccept(item.paper_id)}
+              style="background-color:green;">Accept</button
+            >
           </div>
-
         </div>
-         {/each}
-        
-      </div>
-    </main>
-  {/if}
-  
-  <style>
-    main {
-      max-width: 90%;
-      margin: 2% auto;
-    }
-  
-    h1 {
-    }
+      {/each}
+    </div>
+  </main>
+{/if}
 
-    button {
-    
+<style>
+  main {
+    max-width: 90%;
+    margin: 2% auto;
+  }
+
+  h1 {
+  }
+
+  button {
     margin: 5% 20px;
     padding: 8px 20px;
     border: none;
@@ -130,9 +126,9 @@
     color: #fff;
     font-size: 16px;
     cursor: pointer;
-    }
+  }
 
-    .two-column {
+  .two-column {
     margin-top: -1%;
     /* column-count: 2; /* Split into two columns */
     /* column-gap: 30%; Adjust gap between columns */
@@ -146,20 +142,18 @@
 
   .border_style {
     color: #333;
-      /* Add the border property to your existing class */
-      border: 2px solid #000;
-      /* Optional: Add padding to the content inside the div */
-      padding: 10px;
-      /* Additional styles */
-      background-color: #f4f4f4; /* Set background color */
-      text-align: center; /* Set text alignment to center */
-      font-family: Arial, sans-serif; /* Set font family */
-      margin: 20px; /* Add margin around the div */
-    }
+    /* Add the border property to your existing class */
+    border: 2px solid #000;
+    /* Optional: Add padding to the content inside the div */
+    padding: 10px;
+    /* Additional styles */
+    background-color: #f4f4f4; /* Set background color */
+    text-align: center; /* Set text alignment to center */
+    font-family: Arial, sans-serif; /* Set font family */
+    margin: 20px; /* Add margin around the div */
+  }
 
-    .button-container {
+  .button-container {
     text-align: center;
   }
-  
-  </style>
-  
+</style>
