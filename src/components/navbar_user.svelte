34007c32605a16e8628cd9bb9831ca1bbec1eaac
Rifat <1905094@ugrad.cse.buet.ac.bd>
2024-02-09 20:46:16 +0600

    permission co author frontend

--- src/components/navbar_user.svelte
+++ src/components/navbar_user.svelte
@@ -18,6 +18,7 @@
         </li>
         <li><a href="/{user_id}/Request">My Request</a></li>
         <li><a href="/{user_id}/workshop_request">Workshop Request</a></li>
+        <li><a href="/{user_id}/author_request">co_authors Request</a></li>
         <li><a href="/{user_id}/review">My Review</a></li>
         <li><a href="#">My Profile</a></li>
         <li><a href="/">Log out</a></li>
