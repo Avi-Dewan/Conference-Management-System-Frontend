bf12443fe309344dfdb9c7380df0688567d01de3
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-02-11 14:35:34 +0600

    dashboard of unassigned and pending papers from chair

--- src/routes/[user_id]/dashboard/unassigned_papers/+page.svelte
+++ src/routes/[user_id]/dashboard/unassigned_papers/+page.svelte
@@ -6,37 +6,32 @@
 
   const user_id = $page.params.user_id;
 
-  let getConferencesSummaryURL = `http://localhost:3000/conference/${user_id}/all`;
+  let getUnassigned_papersURL = `http://localhost:3000/conference/unassignedPapers/${user_id}`;
 
-  let getConferencesSummary = null;
+  let unassigned_papers = null;
   onMount(async () => {
     try {
       let user_type = sessionStorage.getItem("user_type");
 
-      const response = await fetch(getConferencesSummaryURL);
+      const response = await fetch(getUnassigned_papersURL);
       if (!response.ok) {
         throw new Error("Failed to fetch data");
       }
 
       let data = await response.json();
 
-      getConferencesSummary = data;
+      unassigned_papers = data;
 
-      console.log(data);
+      console.log(unassigned_papers);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   });
 
-  function handleUnassignedReviewer(conference_id) {
-    goto(`/${user_id}/conference/viewUnassignedPapers/${conference_id}`);
+  function handleViewPaper(paper_id) {
+    goto(`/${user_id}/conference/assignReviewer/${paper_id}`);
   }
-  function handlePendingReview(conference_id) {
-    goto(`/${user_id}/conference/viewPendingReviewPapers/${conference_id}`);
-  }
-  function handleViewConference(conference_id) {
-    goto(`/${user_id}/conference/conference_list/all/${conference_id}`);
-  }
+ 
 </script>
 
 <main>
@@ -56,49 +51,26 @@
     </nav>
   </div>
   <div>
-    {#if getConferencesSummary != null}
+    {#if unassigned_papers != null}
       <br />
       <br />
       <br />
       <br />
       <br />
       <hr />
-      <h2>Your conferences:</h2>
+      <h2> Unassigned Papers:</h2>
 
-      {#each getConferencesSummary as item}
+      {#each unassigned_papers as item}
         <div>
           <hr />
-          <h3>Title: {item.conference_title}</h3>
-          <h4>Total submitted papers: {item.submitted_paper_count}</h4>
-          <h4 style="color: red;">
-            Total Papers without any reviewer assigned: {item.paper_count_with_no_reviewer_assigned}
-          </h4>
-          <h4 style="color: red;">
-            Total Papers with pending reviews: {item.paper_count_with_pending_review}
-          </h4>
-          <h4 style="color: black;">
-            Total workshops: {item.workshop_count}
-          </h4>
-
+          <h3>Title: {item.paper_title}</h3>
+          <h4>Authors: {item.authors}</h4>
+          <h4>Related Fields: {item.related_fields}</h4>
           <button
-            on:click={() => {
-              handleViewConference(item.conference_id);
-            }}>View Conference</button
-          >
-          {#if item.paper_count_with_no_reviewer_assigned != 0}
-            <button
               on:click={() => {
-                handleUnassignedReviewer(item.conference_id);
-              }}>View papers with unassigned reviewer</button
+                handleViewPaper(item.paper_id);
+              }}>View details</button
             >
-          {/if}
-          {#if item.paper_count_with_pending_review != 0}
-            <button
-              on:click={() => {
-                handlePendingReview(item.conference_id);
-              }}>View papers with pending reviews</button
-            >
-          {/if}
         </div>
         <hr />
       {/each}
