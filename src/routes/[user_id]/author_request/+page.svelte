1504b2973d7dec110ad241a05137671d973b25bf
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-03-03 09:59:37 +0600

    paper co-author css

--- src/routes/[user_id]/author_request/+page.svelte
+++ src/routes/[user_id]/author_request/+page.svelte
@@ -6,6 +6,7 @@
   import { goto } from "$app/navigation";
 
   import NavbarUser from "/src/components/navbar_user.svelte";
+  import "/src/app.css";
 
   let user_id, user, data;
 
@@ -98,24 +99,35 @@
     <br />
     <h1>Paper For Co-Authorship</h1>
 
+    <div class="header">
+      {#if data.length == 0}
+        <h2  style="margin-top: 30px;">You have no co-authorship request</h2>
+      {/if}
+    </div>
     <!-- <button on:click={handleClick}>Go to Another Page</button> -->
 
     <div class="cards">
       {#each data as item}
-        <div class="border_style">
-          <h2>{item.paper_title}</h2>
-          <!-- <h3>Pdf Link: {item.pdf_link}</h3> -->
-          <a href={item.pdf_link}>View Paper</a>
-          <h3>Related Fields: {item.related_fields}</h3>
-          <h3>Abstract: {item.abstract}</h3>
-          <a
-            href="/{user_id}/conference/conference_list/all/{item.conference_id}"
-            >View Conference</a
-          >
+      <div class="card bg-gray-200 shadow-xl mt-10">
+        <div class="card-body">
+          <h2 style="margin-top: 20px;">{item.paper_title}</h2>
 
+          <div style="margin-top: 20px;" class="card-actions justify">
+            <a class="btn btn-info" href={item.pdf_link}>View Paper</a>
+          </div>
+
+          <h3 style="margin-top: 20px;">Related Fields: {item.related_fields}</h3>
+          <h3 style="margin-top: 20px;">Abstract: {item.abstract}</h3>
+          
+          <div style="margin-top: 20px;" class="card-actions justify">
+            <a  class="btn btn-neutral"
+              href="/{user_id}/conference/conference_list/all/{item.conference_id}"
+              >View Conference</a
+            >
+          </div>  
           <div
             class="two-column"
-            style="display: block;margin-top:2%"
+            style="display: block;margin-top:0%"
             button-container
           >
             <button
@@ -128,6 +140,7 @@
             >
           </div>
         </div>
+      </div>
       {/each}
     </div>
   </main>
@@ -143,7 +156,7 @@
   }
 
   button {
-    margin: 5% 20px;
+    margin: 3% 4px;
     padding: 8px 20px;
     border: none;
     border-radius: 4px;
