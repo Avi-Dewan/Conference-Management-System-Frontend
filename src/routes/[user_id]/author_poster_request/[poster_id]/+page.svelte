f73eb8a96a096bdf5316ffe8e39e97f5ef0c818c
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-03-03 21:12:23 +0600

    individual request

--- src/routes/[user_id]/author_poster_request/[poster_id]/+page.svelte
+++ src/routes/[user_id]/author_poster_request/[poster_id]/+page.svelte
@@ -6,6 +6,7 @@
     import { goto } from "$app/navigation";
   
     import NavbarUser from "/src/components/navbar_user.svelte";
+    import "/src/app.css";
   
     let user_id, user, data;
     let unreadCount = null;
@@ -87,20 +88,26 @@
   
       <div class="cards">
         {#each data as item}
-          <div class="border_style">
-            <h2>{item.poster_title}</h2>
+        <div class="card bg-gray-200 shadow-xl mt-10">
+          <div class="card-body">
+            <h2 style="margin-top: 20px;">{item.poster_title}</h2>
             <!-- <h3>Pdf Link: {item.pdf_link}</h3> -->
-            <a href={item.pdf_link}>View Poster</a>
-            <h3>Related Fields: {item.related_fields}</h3>
-            <h3>Abstract: {item.abstract}</h3>
-            <a
-              href="/{user_id}/conference/conference_list/all/{item.conference_id}"
-              >View Conference</a
-            >
+            <div style="margin-top: 20px;" class="card-actions justify">
+              <a class="btn btn-info" href={item.pdf_link}>View Poster</a>
+            </div>
+            <h3 style="margin-top: 20px;">Related Fields: {item.related_fields}</h3>
+            <h3 style="margin-top: 20px;">Abstract: {item.abstract}</h3>
+            <div style="margin-top: 20px;" class="card-actions justify">
+            
+              <a class="btn btn-neutral"
+                href="/{user_id}/conference/conference_list/all/{item.conference_id}"
+                >View Conference</a
+              >
+            </div>
   
             <div
               class="two-column"
-              style="display: block;margin-top:2%"
+              style="display: block;margin-top:0%"
               button-container
             >
               <button
@@ -113,6 +120,7 @@
               >
             </div>
           </div>
+        </div>
         {/each}
       </div>
     </main>
