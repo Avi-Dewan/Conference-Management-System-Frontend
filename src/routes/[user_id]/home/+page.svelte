a8b60a646d273041a1e4e45014c556a12e935b76
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-03-03 23:50:53 +0600

    Home page

--- src/routes/[user_id]/home/+page.svelte
+++ src/routes/[user_id]/home/+page.svelte
@@ -9,6 +9,36 @@
 
   let unreadCount = null;
 
+  const features = [
+    {
+      title: 'Create Conference',
+      description: 'Easily create and manage your own conferences.'
+    },
+    {
+      title: 'Submit paper, poster on conference',
+      description: 'Submit your research paper, poster for review on a conference.'
+    },
+    {
+      title: 'Review Papers',
+      description: 'Review papers submitted by others.'
+    },
+  ];
+
+  const features2 = [
+    {
+      title: 'Assign reviewers',
+      description: 'Assign reviewers to review papers while maintaining constrains.'
+    },
+    {
+      title: 'Create workshop on a conference',
+      description: 'Create workshop and assign instructors on them easily.'
+    },
+    {
+      title: 'Manage deadline and important dates',
+      description: 'Manage important dates and deadlines automatically.'
+    }
+  ]
+
   onMount(async () => {
     try {
       const tokenResponse = await fetch(
@@ -56,23 +86,46 @@
     {:else}
       <NavbarUser myVariable={unreadCount} />
     {/if}
+    
+  
+    <h1 style="margin-top: 50px;">Welcome to Conference Management System</h1>
 
-    <button class="btn btn-accent">Accent</button>
-    <h1
-      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-    >
-      Regain <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500"
-        >control</mark
-      > over your days
-    </h1>
-    <h1>Hello world</h1>
-    <h1>Welcome to Conference Management System</h1>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde ipsum,
-      numquam aspernatur earum dolore, libero pariatur voluptatibus itaque qui
-      illo alias, tempore ullam? Perferendis mollitia obcaecati pariatur
-      molestias exercitationem. Nam?
-    </p>
+    <h3 style="margin-top: 40px;">Experience seamless conference management like never before! </h3>
+      <h3>Our system simplifies the process, saving you time and effort. </h3>
+      <h3>Whether you're creating a conference, submitting a paper, or reviewing submissions, we've got you covered.</h3>
+      <h3>Enjoy a hassle-free experience with our conference management system.</h3>
+        
+
+    <br>
+
+    <h2 style="margin-top: 50px; align: center"> Our Features: </h2>
+
+    <br>
+    <br>
+    <section class="features">
+      
+      {#each features as feature}
+        <div class="feature">
+          <h3>{feature.title}</h3>
+          <p>{feature.description}</p>
+        </div>
+      {/each}
+    </section>
+
+    <br>
+    <br>
+
+    <section class="features">
+      
+      {#each features2 as feature}
+        <div class="feature">
+          <h3>{feature.title}</h3>
+          <p>{feature.description}</p>
+        </div>
+      {/each}
+    </section>
+
+
   </main>
 {/if}
 
@@ -82,6 +135,22 @@
     margin: 2% auto;
   }
 
+  
+
   h1 {
+  }
+
+  .features {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 20px;
+  }
+
+  .feature {
+    border: 1px solid #535151;
+    background-color: #beb3b3;
+    padding: 20px;
+    border-radius: 5px;
+    min-height: 150px;
   }
 </style>
